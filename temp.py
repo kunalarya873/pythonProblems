de5b3c43481e9aa9384cfc060a9a6619d7bc885b
kunalarya873 <kunalarya873@gmail.com>
2023-12-27 14:53:06 +0530

    Nothing new

--- temp.py
+++ temp.py
@@ -13,3 +13,27 @@ for ls in (fruit_list1, fruit_list2, fruit_list3):
         sum += 20
  
 print(sum)
+
+init_tuple = ()
+print (init_tuple.__len__())
+
+init_tuple_a = 'a', 'b'
+init_tuple_b = ('a', 'b')
+ 
+print(init_tuple_a == init_tuple_b)
+
+init_tuple_a = '1', '2'
+init_tuple_b = ('3', '4')
+ 
+print (init_tuple_a + init_tuple_b)
+
+init_tuple_a = 1, 2
+init_tuple_b = (3, 4)
+ 
+[print(sum(x)) for x in [init_tuple_a + init_tuple_b]]
+
+init_tuple = [(0, 1), (1, 2), (2, 3)]
+ 
+result = sum(n for _, n in init_tuple)
+ 
+print(result)
